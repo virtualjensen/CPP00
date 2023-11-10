693fc2983b34aad7e91e987a08f2ed95fc896562
jens8n <jensen.bucoy@gmail.com>
2023-11-10 19:54:19 +0400

    starting cpp01

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jebucoy <jebucoy@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 19:36:34 by jebucoy           #+#    #+#             */
+/*   Updated: 2023/11/10 19:53:55 by jebucoy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <iostream>
+
+class Zombie{
+	private:
+		std::string name;
+	public:
+		void	announce(void);
+};
+
+#endif
