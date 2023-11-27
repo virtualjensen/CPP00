0dfb152efd6c848c4099752ae0feab9cb0a8bd30
jens8n <jensen.bucoy@gmail.com>
2023-11-27 12:07:08 +0400

    cpp03ex00 WIP

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,14 +6,16 @@
 /*   By: jebucoy <jebucoy@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 11:48:00 by jebucoy           #+#    #+#             */
-/*   Updated: 2023/11/27 11:59:43 by jebucoy          ###   ########.fr       */
+/*   Updated: 2023/11/27 12:05:56 by jebucoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(){}
+ClapTrap::ClapTrap(){} // default constructor
 
-ClapTrap::ClapTrap(std::string name) : Name(name){}
+ClapTrap::ClapTrap(std::string name) : Name(name){} // parameterized constructor
 
-ClapTrap::~ClapTrap(){}
+ClapTrap& ClapTrap::operator=(const ClapTrap &copy) : () {}
+
+ClapTrap::~ClapTrap(){} // default destructor
